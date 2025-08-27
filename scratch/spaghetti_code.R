eab786e80134afdd634ec5a5f78883710205ed9b
lsdixit <dixitleela@gmail.com>
2025-08-27 15:45:48 -0700

    combined data frames (See #1)

--- scratch/spaghetti_code.R
+++ scratch/spaghetti_code.R
@@ -109,9 +109,9 @@ data_k_loop <-
 #########################################################-
 #########################################################-
 
-source("moving_average.R")
+source(here("R", "moving_average.R"))
 
-
+# PRM site
 PRM$no3_n <- sapply(
   PRM$sample_date,
   moving_average,
@@ -160,3 +160,124 @@ PRM <- PRM %>%
 ggplot(PRM, aes(x = sample_date, y = concentration)) +
   geom_line() +
   facet_wrap(~ions)
+
+# Q1 sites
+Q1$no3_n <- sapply(
+  Q1$sample_date,
+  moving_average,
+  dates = Q1$sample_date,
+  conc = Q1$no3_n,
+  win_size_wks = 9
+)
+Q1$k <- sapply(
+  Q1$sample_date,
+  moving_average,
+  dates = Q1$sample_date,
+  conc = Q1$k,
+  win_size_wks = 9
+)
+Q1$mg <- sapply(
+  Q1$sample_date,
+  moving_average,
+  dates = Q1$sample_date,
+  conc = Q1$mg,
+  win_size_wks = 9
+)
+Q1$ca <- sapply(
+  Q1$sample_date,
+  moving_average,
+  dates = Q1$sample_date,
+  conc = Q1$ca,
+  win_size_wks = 9
+)
+Q1$nh4_n <- sapply(
+  Q1$sample_date,
+  moving_average,
+  dates = Q1$sample_date,
+  conc = Q1$nh4_n,
+  win_size_wks = 9
+)
+# Q2 sites
+Q2$no3_n <- sapply(
+  Q2$sample_date,
+  moving_average,
+  dates = Q2$sample_date,
+  conc = Q2$no3_n,
+  win_size_wks = 9
+)
+Q2$k <- sapply(
+  Q2$sample_date,
+  moving_average,
+  dates = Q2$sample_date,
+  conc = Q2$k,
+  win_size_wks = 9
+)
+Q2$mg <- sapply(
+  Q2$sample_date,
+  moving_average,
+  dates = Q2$sample_date,
+  conc = Q2$mg,
+  win_size_wks = 9
+)
+Q2$ca <- sapply(
+  Q2$sample_date,
+  moving_average,
+  dates = Q2$sample_date,
+  conc = Q2$ca,
+  win_size_wks = 9
+)
+Q2$nh4_n <- sapply(
+  Q2$sample_date,
+  moving_average,
+  dates = Q2$sample_date,
+  conc = Q2$nh4_n,
+  win_size_wks = 9
+)
+# Q3
+Q3$no3_n <- sapply(
+  Q3$sample_date,
+  moving_average,
+  dates = Q3$sample_date,
+  conc = Q3$no3_n,
+  win_size_wks = 9
+)
+Q3$k <- sapply(
+  Q3$sample_date,
+  moving_average,
+  dates = Q3$sample_date,
+  conc = Q3$k,
+  win_size_wks = 9
+)
+Q3$mg <- sapply(
+  Q3$sample_date,
+  moving_average,
+  dates = Q3$sample_date,
+  conc = Q3$mg,
+  win_size_wks = 9
+)
+Q3$ca <- sapply(
+  Q3$sample_date,
+  moving_average,
+  dates = Q3$sample_date,
+  conc = Q3$ca,
+  win_size_wks = 9
+)
+Q3$nh4_n <- sapply(
+  Q3$sample_date,
+  moving_average,
+  dates = Q3$sample_date,
+  conc = Q3$nh4_n,
+  win_size_wks = 9
+)
+# join code
+full_data <- Q1 %>% 
+  full_join(Q2) %>% 
+  full_join(Q3) %>% 
+  full_join(PRM) %>% 
+  pivot_longer("no3_n":"nh4_n", # pivot concentrations and values to be long
+               names_to = "ions",
+               values_to = "concentration")
+
+ggplot(full_data, aes(x = sample_date, y = concentration)) +
+  geom_line(aes(color = sample_id)) +
+  facet_wrap(~ions, ncol = 1, scales = "free_y")
